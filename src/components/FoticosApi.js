f5731084565e8746beda82218cf2eb0058bbf8a2
LizethPatino <lizeth@outlook.com>
2018-12-26 15:35:55 -0500

    adicion de una api de ejemplo

---
+++ src/components/FoticosApi.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+
+
+class FoticosApi extends Component {
+    constructor () {
+        super();
+        this.state = {
+            pictures: [],
+        };
+    }
+
+componentWillMount(){
+    fetch('https://randomuser.me/api/?results=500').then(results =>{
+        return results.json();
+    }).then(data =>{
+        let pictures = data.results.map((pic) =>{
+            return(
+                <div key={pic.results}>
+                    <img src={pic.picture.medium} alt="fotico" />
+                </div>
+            )
+        })
+        this.setState({pictures: pictures});
+    })
+}
+
+render(){
+    return(
+        <div className="container">
+            {this.state.pictures}
+        </div>    
+    )
+}
+}
+
+export default FoticosApi;
